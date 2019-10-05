8394681f7d08669a4c986622139fdc7f346a7c84
johan Bautista <jsbparra@gmail.com>
2019-10-05 06:57:13 +0200

    proy. to do it. form y submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import'./App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: [],
+    };
+  }
+
+  onChange = (event) =>{
+    this.setState({term: event.target.value});
+  }
+
+
+  onSubmit =(event) => {
+    event.preventDefault();
+    this.setState({
+      term:'',
+      items: [ ...this.state.items, this.state.term]
+    });
+  }
+
+ 
+
+render() {
+    return(
+  <div>
+     <form className='App' onSubmit={this.onSubmit}>
+  <input value={this.state.term} onChange = {this.onChange} />
+  <button>Submit</button>
+  </form>
+  <List items={this.state.items}/>
+  </div>
+
+    );
+  }
 }
 
 export default App;
